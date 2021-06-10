856262d81bf57f65b432d4ed997b255187d22c58
maruf786 <maruf@modello.my>
2021-06-10 23:32:53 +0600

    menu update

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,20 +39,8 @@ const Header = ()=>{
       <li className="nav-item">
         <a className="nav-link" href="#"><i className="fas fa-shopping-cart"></i></a>
       </li>
-      <li className="nav-item dropdown">
-        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">Action</a>
-          <a className="dropdown-item" href="#">Another action</a>
-          <div className="dropdown-divider"></div>
-          <a className="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
+     
+
     </ul>
 
   </div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Menu = ()=>{
 
     return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg" style={{background:'#555',color:'#fff'}}>
    
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -11,19 +11,40 @@ const Menu = ()=>{
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          <a className="nav-link" href="#">Weekly Ad <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link" href="#">Sell on Smartcart</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">Investor</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a className="nav-link" href="#">Factory Direct</a>
         </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Farmers Direct</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Export</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Travel/Ticket</a>
+        </li>
+    
       </ul>
+
+      <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <a href="" className="btn btn-danger">TREASURE HUNT</a>
+      </li>
+      <li className="nav-item">
+      Shop super deal
+      </li>
+      </ul>
+      
     </div>
+ 
   </nav>  
   )
 }
